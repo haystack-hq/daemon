edc69dc89b8d2bb93e847250a5988337c06f8de3
Mac McClain <macmcclain@Macs-MacBook-Pro.local>
2018-03-15 14:33:37 -0700

    added dynamic userpath to db store. added jsuck package.

--- src/db/db-conn.js
+++ src/db/db-conn.js
@@ -1,6 +1,12 @@
 #! /usr/bin/env node
+var homedir = require("homedir");
+const fs = require('fs-extra');
+
 var db = require('diskdb');
-var basePath = "/Users/macmcclain/.haystack/client-agent-db"; //todo: replace this hard coded path
+var basePath = homedir() + "/.haystack/client-agent-db";
+
+fs.ensureDirSync(basePath);
+
 db.connect(basePath, ['stacks']);
 
 module.exports = db;
